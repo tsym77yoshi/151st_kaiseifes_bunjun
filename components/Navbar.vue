a624ae4ef00489d4f90cf6ab4f62f74acbf3ca95
tsym77yoshi <67378554+tsym77yoshi@users.noreply.github.com>
2021-10-25 22:23:34 +0900

    [first]

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav_back">
+    <h1 class="nav_title">151st開成祭文化祭準備委員会</h1>
+  </div>
+</template>
+
+<style>
+.nav_back{
+  position:relative;
+  width:100%;
+  background-color:#e4e4e4;
+  z-index:10;
+}
+.nav_title{
+  margin:auto;
+  padding:5px;
+  max-width:600px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="whole" style="position:relative">
+    <h1>タイトル</h1>
+    <p>記事名</p>
+    <p>執筆</p>
+    <div style="height:800px"><p>image</p></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.whole{
+  margin:auto;
+  background-color:white;
+  box-shadow:0px 0px 10px 5px gray;
+  max-width:800px;
+}
+</style>
