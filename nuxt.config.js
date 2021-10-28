bc1a9652d0da08d6c90525bc0dcab88fde97ee9a
tsym77yoshi <67378554+tsym77yoshi@users.noreply.github.com>
2021-10-28 14:17:34 +0900

    [reset]GitHubPagesの利用を取り消し

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '151st_kaiseifes_bunjun/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
@@ -40,7 +40,4 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
-   router: {
-    base: '/151st_kaiseifes_bunjun/'
-  },
 }
