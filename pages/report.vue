4eab193390b4868e7e20d1241f12555c3bc06133
tsym77yoshi <67378554+tsym77yoshi@users.noreply.github.com>
2021-12-22 21:17:49 +0900

    [add]第二回文準通信

--- pages/report.vue
+++ pages/report.vue
@@ -3,6 +3,17 @@
     <div class="whole">
       <div style="width:100%;margin:0;background-color:#ee8888;text-align:center"><a href="https://forms.gle/oN1f6ecxgWkWeWPW7" style="color:black"><p style="font-size:1.5em">文化祭意見 BOXはこちら</p></a></div>
       <img src="~assets/table.svg" alt="・" style="height:40px">
+      <h1 style="display: inline-block;">第二回文準通信</h1>
+      <div class="container">
+        <div class="row" style="text-align:center">
+            <NewsA link="/report/文準通信/文準通信-2nd-net.pdf" picture_name="2_report.png" picture_alt="第二回文準通信カラー版[PDF]"/>
+            <NewsLink link="/hp/kouhou/course2/" picture_name="2_hp.png" picture_alt="htmlの書き方発展（HP講座第二回）"/>
+          </div>
+        <div style="width:100%;margin:0;background-color:#aaaaff;"><a href="https://forms.gle/Rtu9vHCWLH2T6e749" style="color:black"><p style="font-size:1em">中後夜祭企画案募集フォーム[GoogleForm]</p></a></div>
+        <p style="margin:0">※動画講座は参説資料作成で忙しかった為、今回はお休みさせていただきます</p>
+      </div>
+      <hr size=10 color="black" class="hrLine">
+      <img src="~assets/table.svg" alt="・" style="height:40px">
       <h1 style="display: inline-block;">第一回文準通信</h1>
       <div class="container"><div class="row" style="text-align:center">
         <NewsA link="/report/文準通信/文準通信-1st-net.pdf" picture_name="1_report.png" picture_alt="第一回文準通信カラー版[PDF]"/>
@@ -10,7 +21,6 @@
         <NewsLink link="/movie/course1/" picture_name="1_movie.png" picture_alt="動画編集のススメ（動画講座第一回）"/>
         <NewsA link="/other/Illustrator.pdf" picture_name="1_illustrator.png" picture_alt="Illustrator講座完全版[PDF]"/>
       </div></div>
-      <div style="height:50vh"></div>
       <div style="width:100%;margin:0;background-color:#ee8888;text-align:center"><a href="https://forms.gle/oN1f6ecxgWkWeWPW7" style="color:black"><p style="font-size:1.5em">文化祭意見 BOXはこちら</p></a></div>
       <p style="text-indent:1em">このページには文準通信を載せていきます。また、文準通信の回ごとにリンクは変える予定はありません。中身をこのページに追加していきます。</p>
     </div>
@@ -23,3 +33,4 @@ export default {}
 
 <style>
 </style>
+
